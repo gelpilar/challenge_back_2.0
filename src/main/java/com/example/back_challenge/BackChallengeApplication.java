59525d1efe33eb5e1545dfbace0a62e404c26a1e
gelpilar <pilarscoll@gmail.com>
2025-01-12 23:26:35 -0300

    actualizacion 2

--- src/main/java/com/example/back_challenge/BackChallengeApplication.java
+++ src/main/java/com/example/back_challenge/BackChallengeApplication.java
@@ -19,7 +19,7 @@ public class BackChallengeApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("https://challenge-back-2-0.onrender.com")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 							.allowCredentials(true);
